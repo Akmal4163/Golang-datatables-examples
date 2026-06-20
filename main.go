1d03ac31387084fa5e095e1f4ad441d45cf021ff
Akmal4163 <bebekngayang@gmail.com>
2026-06-20 07:48:12 +0700

    remove pointers from db

--- main.go
+++ main.go
@@ -5,13 +5,122 @@ import (
 	"database/sql"
 	"log"
 	"html/template"
-	"example/datatables/handlers"
+	"encoding/json"
+	"fmt"
+	"strconv"
 	_ "modernc.org/sqlite"
 
 )
 
+
 type Data struct {
 	Title string
+}
+
+type Barang struct {
+	ID 				int 	`json:"id"`
+	KodeBarang 		string	`json:"kode_barang"`
+	NamaBarang 		string	`json:"nama_barang"`
+	Satuan 			string	`json:"satuan"`
+	HargaSatuan 	float32	`json:"harga_satuan"`
+	Jumlah 			float32	`json:"jumlah"`
+	Keterangan 		string	`json:"keterangan"`
+}
+
+type DataTables struct {
+	Draw            int        `json:"draw"`
+	RecordsTotal    int        `json:"recordsTotal"`
+	RecordsFiltered int        `json:"recordsFiltered"`
+	Data            []Barang   `json:"data"`
+}
+
+
+func datatableHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+	draw, _ := strconv.Atoi(r.URL.Query().Get("draw"))
+	start, _ := strconv.Atoi(r.URL.Query().Get("start"))
+	length, _ := strconv.Atoi(r.URL.Query().Get("length"))
+	searchQuery := r.URL.Query().Get("search[value]")
+	orderColumnIndex := r.URL.Query().Get("order[0][column]")
+	orderColumnDir := r.URL.Query().Get("order[0][dir]")
+
+	if length <= 0 {
+		length = 10
+	}
+
+	columnMap := map[string]string {
+		"0": "id",
+		"1": "kode_barang",
+		"2": "nama_barang",
+		"3": "satuan",
+		"4": "harga_satuan",
+		"5": "jumlah",
+		"6": "keterangan",
+	}
+
+	orderByCol, validOrder := columnMap[orderColumnIndex]
+	if !validOrder {
+		orderByCol = "id"
+	}
+
+	if orderColumnDir != "asc" && orderColumnDir != "desc" {
+		orderColumnDir = "asc"
+	}
+
+	var recordsTotal int
+	err := db.QueryRow("SELECT COUNT(id) FROM master_barang").Scan(&recordsTotal)
+	if err != nil {
+		http.Error(w, "Error:", http.StatusInternalServerError)
+		return
+	}
+
+	querySelect := fmt.Sprintf(`
+		SELECT id, kode_barang, nama_barang, satuan, harga_satuan, jumlah, keterangan 
+		FROM master_barang 
+		WHERE kode_barang LIKE ? OR nama_barang LIKE ? OR satuan LIKE ? OR harga_satuan LIKE ?
+		OR jumlah LIKE ? OR keterangan LIKE ? ORDER BY %s %s
+		LIMIT ? OFFSET ?;`, orderByCol, orderColumnDir)
+
+	searchPattern := "%" + searchQuery + "%"
+	rows, err := db.Query(querySelect, searchPattern, searchPattern, searchPattern, searchPattern, searchPattern, searchPattern, length, start)
+	if err != nil {
+		http.Error(w, "Error:", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+	defer rows.Close()
+
+	listBarang := []Barang{}
+	for rows.Next() {
+		var b Barang
+		err := rows.Scan(&b.ID, &b.KodeBarang, &b.NamaBarang, &b.Satuan, &b.HargaSatuan, &b.Jumlah, &b.Keterangan)
+		if err != nil {
+			http.Error(w, "Error:", http.StatusInternalServerError)
+			return
+		}
+		listBarang = append(listBarang, b)
+	}
+
+	
+	queryCountFilter := `SELECT COUNT(id) FROM master_barang 
+		WHERE kode_barang LIKE ? OR nama_barang LIKE ? OR satuan LIKE ? OR harga_satuan LIKE ?
+		OR jumlah LIKE ? OR keterangan LIKE ?;`
+	
+	var recordsFiltered int
+	err = db.QueryRow(queryCountFilter, searchPattern, searchPattern, searchPattern, searchPattern, searchPattern, searchPattern).Scan(&recordsFiltered)
+	if err != nil {
+		http.Error(w, "Error:", http.StatusInternalServerError)
+		return
+	}
+
+	response := DataTables{
+		Draw: draw,
+		RecordsTotal: recordsTotal,
+		RecordsFiltered: recordsFiltered,
+		Data: listBarang,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
@@ -21,13 +130,11 @@ func main() {
 	}
 	defer db.Close()
 
-	datatableHandler := &handlers.DatatableHandlers{
-		DB: db,
-	}
-
 	tmpl := template.Must(template.ParseFiles("templates/datatables.html"))
 
-	http.HandleFunc("/api/barang", datatableHandler.GetData)
+	http.HandleFunc("/api/barang", func(w http.ResponseWriter, r *http.Request) {
+        datatableHandler(db, w, r)
+    })
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         data := Data{
 			Title: "datatables example",
@@ -35,5 +142,6 @@ func main() {
 		tmpl.Execute(w, data)
     })
 
+	fmt.Println("server Listen at Port 8080")
     http.ListenAndServe(":8080", nil)
 }
